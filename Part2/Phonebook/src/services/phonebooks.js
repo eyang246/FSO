8ab00ad5b14e818da286f244e399fc91d6af92f4
Eric Yang <eyang246@hawaii.edu>
2023-07-24 11:00:33 -1000

    fixed frontend to work with backend changes

--- Part2/Phonebook/src/services/phonebooks.js
+++ Part2/Phonebook/src/services/phonebooks.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-// const baseUrl = 'https://phonebook-backend-xm28.onrender.com/api/persons'
-const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = 'https://phonebook-backend-xm28.onrender.com/api/persons'
+// const baseUrl = 'http://localhost:3001/api/persons'
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
